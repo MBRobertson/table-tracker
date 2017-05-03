ebcc3f0e982ad5af858dd61eca9a04e4ea434a3e
MBRobertson <mbr.robertson@gmail.com>
2017-05-03 13:18:46 +1200

    Added multiple connection states

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,19 @@ socket.on('update', function(data) {
     $('.beacon').remove();
     for (var b in data)
     {
-        $('.beacon-list').append("<li class='beacon'><span class='id'>" + b + "</span><span class='used " + (data[b] ? "inuse" : "free") + "'><span class='indicator'></span>" + (data[b] ? "Phone Nearby" : "Nothing Nearby") + "<span></li>")
+        var usage = "free";
+        var text = "Nothing Nearby";
+        if (data[b] == 0)
+        {
+            usage = "wait";
+            text = "Phone Nearby";
+        }
+        else if (data[b] == 1)
+        {
+            usage = "";
+            text = "Phone Nearby";
+        }
+        $('.beacon-list').append("<li class='beacon'><span class='id'>" + b + "</span><span class='used " + usage + "'><span class='indicator'></span>" + text + "<span></li>")
     }
     if ($('.beacon').length == 0)
     {
