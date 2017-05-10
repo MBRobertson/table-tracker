969571128840410ede615da76574aea32d55799d
MBRobertson <mbr.robertson@gmail.com>
2017-05-10 02:19:24 +0000

    Postgres integration

--- public/js/main.js
+++ public/js/main.js
@@ -5,22 +5,28 @@ socket.on('update', function(data) {
     $('.beacon').remove();
     for (var b in data)
     {
-        var usage = "free";
-        var text = "Nothing Nearby";
+        var usage = "wait";
+        var text = "WIP";
         // if (data[b] == 0)
         // {
         //     usage = "wait";
         //     text = "Phone Nearby";
         // }
-        if (data[b].devices.length >= 1)
-        {
-            usage = "";
-            text = data[b].devices.join() + " Nearby";
-        }
-        $('.beacon-list').append("<li class='beacon'><span class='id'>" + data[b].beacon + "</span><span class='used " + usage + "'><span class='indicator'></span>" + text + "<span></li>")
+        // if (data[b].devices.length >= 1)
+        // {
+        //     usage = "";
+        //     text = data[b].devices.join() + " Nearby";
+        // }
+        $('.beacon-list').append("<li class='beacon " + data[b].region + "'><span class='id'>" + data[b].name + "</span><span class='used " + usage + "'><span class='indicator'></span>" + text + "<span></li>")
     }
     if ($('.beacon').length == 0)
     {
         $('.beacon-list').append("<li class='beacon no-beacons'>No beacons registered</li>")
     }
+})
+
+socket.on('deviceupdate', function(data) {
+    data.forEach(function(device) {
+        $('.' + device.beacon).append("<p>" + device.device + "</p>");
+    }, this);
 })
