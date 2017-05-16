adfb45e623d06ab0ea27ff6cd65937a083841e85
MBRobertson <mbr.robertson@gmail.com>
2017-05-16 14:19:28 +1200

    Actually fix put requests

--- api/api.js
+++ api/api.js
@@ -7,7 +7,7 @@ var updateHandler = null;
 api.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods: *");
+    res.header("Access-Control-Allow-Methods", "*");
     next();
 });
 
